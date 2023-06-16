f6012deb1d33ad2133df99793593d45e377646cc
truongbx_weupgroup <truongbx@weupgroup.vn>
2023-06-16 17:24:46 +0700

    commit change name

--- lib/src/controls/landscape_player_controls.dart
+++ lib/src/controls/landscape_player_controls.dart
@@ -2,22 +2,18 @@ import 'package:flick_video_player_custom/flick_video_player_custom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:video_player/video_player.dart';
 
 import '../sheet/setting_sheet.dart';
 import 'play_toggle.dart';
 
 class LandscapePlayerControls extends StatelessWidget {
-  const LandscapePlayerControls(
-      {Key? key, this.iconSize = 20, this.fontSize = 12})
-      : super(key: key);
+  const LandscapePlayerControls({Key? key, this.iconSize = 24, this.fontSize = 14}) : super(key: key);
   final double iconSize;
   final double fontSize;
 
   @override
   Widget build(BuildContext context) {
-    FlickVideoManager flickVideoManager =
-        Provider.of<FlickVideoManager>(context);
+    FlickVideoManager flickVideoManager = Provider.of<FlickVideoManager>(context);
     return FlickShowControlsAction(
       child: FlickAutoHideChild(
         child: Stack(
@@ -30,106 +26,93 @@ class LandscapePlayerControls extends StatelessWidget {
               ),
             ),
             Positioned.fill(
-              child: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Container(),
-                  ),
-                  Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        FlickPlayToggle(
-                          size: 20,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        FlickCurrentPosition(
-                          fontSize: fontSize,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Expanded(
-                          child: Container(
-                            child: FlickVideoProgressBar(
-                              flickProgressBarSettings:
-                                  FlickProgressBarSettings(
-                                height: 10,
-                                handleRadius: 10,
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 8.0,
-                                  vertical: 8,
-                                ),
-                                backgroundColor: Colors.white24,
-                                bufferedColor: Colors.white38,
-                                getPlayedPaint: (
-                                    {double? handleRadius,
-                                    double? height,
-                                    double? playedPart,
-                                    double? width}) {
-                                  return Paint()
-                                    ..shader = LinearGradient(colors: [
-                                      Color.fromRGBO(108, 165, 242, 1),
-                                      Color.fromRGBO(97, 104, 236, 1)
-                                    ], stops: [
-                                      0.0,
-                                      0.5
-                                    ]).createShader(
-                                      Rect.fromPoints(
-                                        Offset(0, 0),
-                                        Offset(width!, 0),
-                                      ),
-                                    );
-                                },
-                                getHandlePaint: (
-                                    {double? handleRadius,
-                                    double? height,
-                                    double? playedPart,
-                                    double? width}) {
-                                  return Paint()
-                                    ..shader = RadialGradient(
-                                      colors: [
-                                        Color.fromRGBO(97, 104, 236, 1),
-                                        Color.fromRGBO(97, 104, 236, 1),
-                                        Colors.white,
-                                      ],
-                                      stops: [0.0, 0.4, 0.5],
-                                      radius: 0.4,
-                                    ).createShader(
-                                      Rect.fromCircle(
-                                        center:
-                                            Offset(playedPart!, height! / 2),
-                                        radius: handleRadius!,
-                                      ),
-                                    );
-                                },
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(8,0,8,8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              FlickCurrentPosition(
+                                fontSize: fontSize,
                               ),
-                            ),
+                              Text(
+                                ' / ',
+                                style: TextStyle(color: Colors.white, fontSize: fontSize),
+                              ),
+                              FlickTotalDuration(
+                                fontSize: fontSize,
+                              ),
+                            ],
                           ),
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        FlickTotalDuration(
-                          fontSize: fontSize,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        FlickSoundToggle(
-                          size: 20,
-                        ),
-                      ],
+                          SizedBox(
+                            width: iconSize / 2,
+                          ),
+                          FlickSoundToggle(
+                            size: iconSize,
+                          ),
+                          Expanded(
+                            child: Container(),
+                          ),
+                          FlickSubtitleToggle(
+                            size: iconSize,
+                          ),
+                          SizedBox(
+                            width: iconSize / 2,
+                          ),
+                          FlickFullScreenToggle(
+                            size: iconSize,
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                    FlickVideoProgressBar(
+                      flickProgressBarSettings: FlickProgressBarSettings(
+                        height: 10,
+                        handleRadius: 8,
+                        padding: const EdgeInsets.all(8),
+                        backgroundColor: Colors.white24,
+                        bufferedColor: Colors.white38,
+                        getPlayedPaint: ({double? handleRadius, double? height, double? playedPart, double? width}) {
+                          return Paint()
+                            ..shader = LinearGradient(colors: [Color.fromRGBO(108, 165, 242, 1), Color.fromRGBO(97, 104, 236, 1)], stops: [0.0, 0.5])
+                                .createShader(
+                              Rect.fromPoints(
+                                Offset(0, 0),
+                                Offset(width!, 0),
+                              ),
+                            );
+                        },
+                        getHandlePaint: ({double? handleRadius, double? height, double? playedPart, double? width}) {
+                          return Paint()
+                            ..shader = RadialGradient(
+                              colors: [
+                                Color.fromRGBO(97, 104, 236, 1),
+                                Color.fromRGBO(97, 104, 236, 1),
+                                Colors.white,
+                              ],
+                              stops: [0.0, 0.4, 0.5],
+                              radius: 0.4,
+                            ).createShader(
+                              Rect.fromCircle(
+                                center: Offset(playedPart!, height! / 2),
+                                radius: handleRadius!,
+                              ),
+                            );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Positioned(
-              right: 20,
+              right: 16,
               top: 10,
               child: GestureDetector(
                 onTap: () => onPressSetting(context, flickVideoManager),
@@ -145,14 +128,12 @@ class LandscapePlayerControls extends StatelessWidget {
     );
   }
 
-  void onPressSetting(
-      BuildContext context, FlickVideoManager flickVideoManager) {
+  void onPressSetting(BuildContext context, FlickVideoManager flickVideoManager) {
     showModalBottomSheet(
         context: context,
-        shape:  RoundedRectangleBorder(
+        shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
         ),
-        builder: (context) =>
-            SettingSheet(featureList: flickVideoManager.optionList));
+        builder: (context) => SettingSheet(featureList: flickVideoManager.optionList));
   }
 }
